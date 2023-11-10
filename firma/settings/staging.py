d865b2fbf4f1592c1f5fa65557f7c64fbb22ab6b
Juan Botero <jpillworld@gmail.com>
2023-11-10 16:22:17 -0500

    Revert "Disable Debug in staging"
    This reverts commit 6d1b8d96e40374908e830c5675c5210207ea956d.

--- firma/settings/staging.py
+++ firma/settings/staging.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from firma.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["webapp-firma-backend.azurewebsites.net"]
 CSRF_TRUSTED_ORIGINS = ["https://webapp-firma-backend.azurewebsites.net"]
