76bccfeea872dfb337c04051e8e103f4330d8d0b
Juan Botero <jpillworld@gmail.com>
2023-12-14 11:59:42 -0500

    Add managed=True to Meta class in models.py and remove print statement in azure_services.py

--- utils/azure_services.py
+++ utils/azure_services.py
@@ -5,7 +5,6 @@ from firma.settings.development import AZURE_STORAGE_CONNECTION_STRING, AZURE_ST
 # Create a function con connect to azure blob storage, the credentials are in settings.py
 def connect_to_azure_storage():
     blob_client = BlobServiceClient.from_connection_string(conn_str=settings.AZURE_STORAGE_CONNECTION_STRING)
-    print(f'Connection string: {settings.AZURE_STORAGE_CONNECTION_STRING}')
     return blob_client
 
 # Create function to open connecton to Azure Storage
