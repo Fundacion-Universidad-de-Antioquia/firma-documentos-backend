833e11692453812dc6682cb16ae463ee5ccdc70b
Juan Botero <jpillworld@gmail.com>
2024-05-07 05:58:53 +0500

    Deshabilita Debug

--- firma/settings/staging.py
+++ firma/settings/staging.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from firma.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["backend-intranet.azurewebsites.net"]
 CSRF_TRUSTED_ORIGINS = ["https://backend-intranet.azurewebsites.net"]
